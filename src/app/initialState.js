fe77cf853d497f8320dbedc38316f58e0bcea748
Michael Murwayi <mikemurwayi7@gmail.com>
2023-08-07 07:09:42 -0400

    ft-map: adding initail map view point to global state object

---
+++ src/app/initialState.js
@@ -0,0 +1,12 @@
+
+const initialState = {
+    mapboxtoken: "",
+    initialViewPoint: {
+        latitude: "37.7749",
+        longitude: "-122.4196",
+        zoom:"3",
+    }
+}
+
+export default initialState
+
