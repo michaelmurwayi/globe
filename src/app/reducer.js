ecf78ef32e06f29c8202cca71bfd0467396fdea3
Michael Murwayi <mikemurwayi7@gmail.com>
2023-08-07 07:27:09 -0400

    ft-map: adding a reducer for the map

---
+++ src/app/reducer.js
@@ -0,0 +1,11 @@
+import initialState from "./initialState";
+
+const mapReducer = (state= initialState, action)=>{
+    switch(action.type){
+        default:
+            return state
+    }
+
+}
+
+export default mapReducer
