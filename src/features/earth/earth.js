7ca48ca6f83d52c2b7821ffbbf1b4f830b90dce7
Michael Murwayi <mikemurwayi7@gmail.com>
2023-08-07 03:43:37 -0400

    initial project commit with earth component

---
+++ src/features/earth/earth.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactGlobe from "globe.gl"
+
+const Earth = ()=>{
+    return(
+        
+        <div>
+            <ReactGlobe
+                height="100vh"
+            />
+        </div>
+    )
+}
+
+export default Earth
